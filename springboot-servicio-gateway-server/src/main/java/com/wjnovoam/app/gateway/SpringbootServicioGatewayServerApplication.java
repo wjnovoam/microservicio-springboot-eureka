d61c2f26cbfd8414d05ba6dadd355ec1f4e35ba1
William Novoa <wjnovoam@ufpso.edu.co>
2022-03-03 10:53:57 -0500

    Agregando microservicio cloud-gateway

---
+++ springboot-servicio-gateway-server/src/main/java/com/wjnovoam/app/gateway/SpringbootServicioGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.wjnovoam.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootServicioGatewayServerApplication.class, args);
+    }
+
+}
