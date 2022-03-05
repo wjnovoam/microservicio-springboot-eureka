cadb0429393bb87e54fd0eb041c8b55cbc082d59
William Novoa <wjnovoam@ufpso.edu.co>
2022-03-05 12:45:54 -0500

    Agregando los @value para una peticion de configuracion

--- springboot-servicio-item/src/main/java/com/wjnovoam/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/wjnovoam/app/item/controllers/ItemController.java
@@ -9,10 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -25,6 +30,9 @@ public class ItemController {
     @Autowired
     @Qualifier("serviceRestTemplate")
     private ItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
 
     @GetMapping("/listar")
@@ -81,6 +89,15 @@ public class ItemController {
         item.setProducto(producto);
 
         return CompletableFuture.supplyAsync(() -> item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+
+        return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
 }
